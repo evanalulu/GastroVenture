a1a7457fbaaa01b246faa3c41e129f9fa07626a5
Evana Pradhan <evanapra@gmail.com>
2024-11-09 13:13:13 -0800

    Adding sound effect to player movement - player.js

--- player.js
+++ player.js
@@ -35,10 +35,16 @@ function swimPlayer() {
 
   if (kb.pressing("left")) {
     player.vel.x = -3;
-    if (kb.presses("space")) bulletLaunch("left");
+    if (kb.presses("space")) {
+      bulletLaunch("left");
+      shootSound.play();
+    }
   } else if (kb.pressing("right")) {
     player.vel.x = 3;
-    if (kb.presses("space")) bulletLaunch("right");
+    if (kb.presses("space")) {
+      bulletLaunch("right");
+      shootSound.play();
+    }
   } else if (kb.pressing("up")) {
     player.vel.y = -3;
   } else if (kb.pressing("down")) {
@@ -48,27 +54,38 @@ function swimPlayer() {
     player.vel.y = 0;
     player.changeAni("swimIdle");
   }
+  if (kb.presses("left") || kb.presses("right") || kb.presses("up") || kb.presses("down")) {
+    swimSound.setVolume(0.7);
+    swimSound.play();
+  }
 
   if (player.x > 597) player.x = 597;
   if (player.y > 510) player.y = 510;
   if (player.y < 108) player.y = 108;
 }
 
+let isWalking = false;
 function mazePlayer() {
   if (kb.presses("left")) player.changeAni("lWalk");
   if (kb.presses("right")) player.changeAni("rWalk");
 
-  if (kb.pressing("left")) {
-    player.vel.x = -5;
-  } else if (kb.pressing("right")) {
-    player.vel.x = 5;
-  } else if (kb.pressing("up")) {
-    player.vel.y = -5;
-  } else if (kb.pressing("down")) {
-    player.vel.y = 5;
+  if (kb.pressing("left") || kb.pressing("right") || kb.pressing("up") || kb.pressing("down")) {
+    player.vel.x = kb.pressing("left") ? -5 : kb.pressing("right") ? 5 : player.vel.x;
+    player.vel.y = kb.pressing("up") ? -5 : kb.pressing("down") ? 5 : player.vel.y;
+
+    if (!isWalking) {
+      walkWaterSound.setVolume(0.5);
+      walkWaterSound.loop();
+      walkWaterSound.play();
+      isWalking = true;
+    }
   } else {
     player.vel.x = 0;
     player.vel.y = 0;
-    player.changeAni("idle");
+
+    if (isWalking) {
+      walkWaterSound.stop();
+      isWalking = false;
+    }
   }
 }
