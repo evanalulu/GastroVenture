92e0bf430c69ddc6695efb6e182fa8ee2dc1e42b
Evana Pradhan <evanapra@gmail.com>
2024-11-09 00:48:52 -0800

    Moving dialogue set up into dialogues.js

--- assets.js
+++ assets.js
@@ -161,72 +161,19 @@ function aniSetUp() {
   player.pixelPerfect = true;
 }
 
-function textSetUp() {
-  titleText = createDiv("");
-  titleText.position(width / 2 - 90, height / 2 - 37);
-  titleText.style("font-size", "20px");
-  titleText.style("color", "#ffffff");
-  titleText.addClass("custom-font");
+function backgroundSetUp() {
+  oesophagusBg = new Sprite(oesophagusBackground, -100, -2000, "n");
+  oesophagusBg.scale = zoomLevel;
 
-  enterText = createDiv("");
-  enterText.position(width / 2 - 56, height / 2 + 43);
-  enterText.style("font-size", "12px");
-  enterText.style("color", "#ffffff");
-  enterText.addClass("custom-font");
+  stomachBg = new Sprite(stomachBackground, -1000, 2000, "n");
+  stomachBg.scale = zoomLevel;
 
-  mapText = createDiv("");
-  mapText.position(30, 40);
-  mapText.style("font-size", "15px");
-  mapText.style("color", "#ffffff");
-  mapText.addClass("custom-font");
+  mazeBg = new Sprite(mazeBackground, -1000, 2000, "n");
+  mazeBg.scale = zoomLevel - 0.5;
+
+  largeIntestineBg = new Sprite(largeIntestineBackground, 3000, -2000, "n");
+  largeIntestineBackground.scale = zoomLevel;
+
+  rectumBg = new Sprite(rectumBackground, -3000, 2000, "n");
+  rectumBg.scale = zoomLevel;
 }
-
-// OESOPHAGUS
-let screen1Dialogues = [
-  "Welcome to the esophagus!",
-  "You’re now traveling down through a process \ncalled peristalsis. Peristalsis is a series of \nmuscle contractions that pushes food and \nliquid down towards the stomach.",
-  "In the esophagus, you’re safe for now.\nThere’s no acid here, but once you \nreach the stomach, you’ll need to be \nprepared for a much harsher environment.",
-  "To survive in the stomach’s acidic environment, \nyou’ll need to collect special items \n Look for alkaline crystals as you \ntravel through the esophagus.",
-  "These alkaline crystals will help neutralize the \nstomach’s acid, protecting you from damage.",
-  "Stay alert for any obstacles along the way.\nRemember, the journey through the digestive \nsystem isn’t easy!",
-  "Good luck as you continue your descent!\nWith each step, you’re learning more about \nhow the body digests food.",
-];
-
-// STOMACH
-let screen2Dialogues = [
-  "You’ve entered the stomach!",
-  "The stomach is a powerful organ where digestion \nreally kicks into high gear.",
-  "In here, the stomach releases digestive juices, \nincluding strong acids and enzymes.\nThese break down food into smaller, \nabsorbable nutrients.",
-  "The environment is extremely acidic –\ntoo harsh for most things to survive.\nGood thing you collected those alkaline crystals!\nIt's bubble will help you survive \n for a short time.",
-  "Watch out for enemies!\nThese represent harmful bacteria or pathogens \nthat may try to take you down.",
-  "Press SPACE to shoot them and defend yourself",
-  "Travel down to end after shooting all enemies.\nStay alert and good luck!",
-];
-
-// SMALL INTESTINES
-let screen3Dialogues = [
-  "Welcome to the small intestine!",
-  "This is where most nutrient absorption happens\nas food particles pass through.",
-  "The small intestine is long and winding,\nwhich helps increase the surface area for\nmaximum absorption of nutrients.",
-  "Your mission here is to make it through\nthe maze of the small intestine!",
-  "Good luck! The journey is almost over,\nand you’re one step closer to completing\nthe digestive system journey!",
-];
-
-// LARGE INTESTINES
-let screen4Dialogues = [
-  "Welcome to the large intestine!",
-  "Here, water is absorbed from food waste. \nStrong muscles push the waste toward the rectum.",
-  "Follow the flow to complete your journey!\n Collect as much water as you can to stay hydrated!",
-  "You’re almost at the end of the digestive tract!\n Good luck!",
-];
-
-// RECTUM
-let screen5Dialogues = [
-  "You're in the rectum, the last stretch \nof the journey!",
-  "Run to the end to reach the anus and escape!",
-  "Watch out for obstacles and the \nfinal boss chasing you!",
-  "Almost there—don't let the boss catch you!",
-];
-
-let dialogueIndex = 0;
-let dialogueBox;
